d99a93ed48eeeb39e9a19ad3c7e117618fa04ff4
Rodolfo <chavesrodolfo@gmail.com>
2020-10-12 23:35:56 -0300

    landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
+import './styles/global.css';
+import './styles/pages/landing.css';
+import { FiArrowRight } from 'react-icons/fi';
+
+import logoImg from './images/logo.svg';
+
 function App() {
   return (
-    <div className="App">
-      <h1> Hello World!</h1>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="happy"/>
+
+        <main>
+          <h1>Leve Felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+
+        <div className="location">
+          <strong>Curitiba</strong>
+          <span>Paraná</span>
+        </div>
+
+        <a href="" className="enter-app">
+           <FiArrowRight size={26} color="rgba(0,0,0, 0.6)"/>
+        </a>
+
+      </div>
     </div>
   );
 }
