99b2f643afe928ebe6282a79a961df64f516f344
Rodolfo <chavesrodolfo@gmail.com>
2020-10-14 17:53:04 -0300

    new screens

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,13 +1,22 @@
 import React from 'react';
-import { FiPlus } from 'react-icons/fi';
+import { FiArrowRight, FiPlus } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
 import mapMarkerImg from '../images/map-marker.svg';
+import Leaflet from 'leaflet';
+
 import '../styles/pages/orphanages-map.css';
 
-import { Map, TileLayer } from 'react-leaflet';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
+
+const mapIcon = Leaflet.icon({
+    iconUrl: mapMarkerImg,
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+});
 
 function OrphanagesMap() {
     return(
@@ -26,12 +35,29 @@ function OrphanagesMap() {
                 </footer>
             </aside>
 
-            <Map center={[-25.4394222,-49.2682691]} zoom={14} style={{width: '100%', height: '100%'}}>
+            <Map 
+                center={[-25.4394222,-49.2682691]} 
+                zoom={14} 
+                style={{width: '100%', height: '100%'}}>
+                
                 {/*<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png"/>*/}
+                
                 <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+                
+                <Marker
+                    position={[-25.4394222,-49.2682691]}
+                    icon={mapIcon}
+                >
+                    <Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+                        Instituto AMA
+                        <Link to="/orphanages/1">
+                            <FiArrowRight size={20} color="#fff"/>
+                        </Link>
+                    </Popup>
+                </Marker>
             </Map>
 
-            <Link to="/" className="create-orphanage">
+            <Link to="/orphanages/create" className="create-orphanage">
                 <FiPlus size={32} color="#fff"/>
              </Link>
         </div>
